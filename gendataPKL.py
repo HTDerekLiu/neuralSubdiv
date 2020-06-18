de3a206eaf6b72595e71d1f0e27306cfccc4dbfe
HTDerekLiu <hsuehtil@cs.toronto.edu>
2020-06-18 18:44:46 +0800

    update gendata

--- gendataPKL.py
+++ gendataPKL.py
@@ -5,10 +5,10 @@ from __future__ import print_function
 from include import *
 
 def main():
-    mesh_folders = ['./data_meshes/cartoon_elephant_10/']
+    mesh_folders = ['./data_meshes/cartoon_elephant_200/']
     # mesh_folders = ['./data_meshes/bunny/', './data_meshes/rockerArm/', './data_meshes/fertility/']
     S = TrainMeshes(mesh_folders)
 
-    pickle.dump(S, file = open("./data_PKL/cartoon_elephant_valid.pkl", "wb"))
+    pickle.dump(S, file = open("./data_PKL/cartoon_elephant_train.pkl", "wb"))
 if __name__ == '__main__':
     main()
